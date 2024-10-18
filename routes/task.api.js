a42a815369b575243f6613e3f9bee29f45484bc2
parksojeong <sojeong9432@gmail.com>
2024-10-19 01:30:29 +0900

    fix: get요청 확인용 코드 수정

--- routes/task.api.js
+++ routes/task.api.js
@@ -4,7 +4,10 @@ const router = express.Router()
 
 router.post('/', taskController.createTask);
 
-router.get('/',taskController.getTask)
+// router.get('/',taskController.getTask)
+router.get('/',(req,res)=>{
+    res.send('API is running')
+})
 
 router.put('/:id',taskController.updateTask)
 
