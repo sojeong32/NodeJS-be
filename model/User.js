2db2f625feeac6a7de8f1c3f80bfc4dbdbc29212
박소정 <thwjd0235@naver.com>
2024-10-21 15:48:55 +0900

    feat: 회원가입 구현

--- model/User.js
+++ model/User.js
@@ -1,10 +1,23 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = Schema({
+const userSchema = Schema(
+  {
     name: {
-        type: String,
-        require: true,
-    }
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-})
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- routes/user.api.js
+++ routes/user.api.js
@@ -1,9 +1,8 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+const userController = require("../controller/user.controller");
 
 // 1. 회원가입 endpoint
-router.post("/", (req, res)=> {
-    res.send('create user controller will be here')
-})
+router.post("/", userController.createUser);
 
 module.exports = router;
