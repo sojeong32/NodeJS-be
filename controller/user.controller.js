a1023aa6e90718b196bcf1ae93e9185756f30dca
박소정 <thwjd0235@naver.com>
2024-10-22 22:03:56 +0900

    fix: 회원가입 시 이메일, 이름이 없을 때 에러메세지 보내기

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -8,6 +8,12 @@ userController.createUser = async (req, res) => {
   try {
     const { email, name, password } = req.body;
     const user = await User.findOne({ email: email });
+    if (!email) {
+      throw new Error("이메일을 입력하세요.");
+    }
+    if (!name) {
+      throw new Error("이름을 입력하세요.");
+    }
     if (!password) {
       throw new Error("비밀번호를 입력하세요.");
     }
